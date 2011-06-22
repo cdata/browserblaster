e335754aa675bfaf289fa7baf15d7583d606ccdb
Christopher Joel <chris@cloudflare.com>
2011-06-22 16:59:27 -0700

    Added share functionality back in..

--- browserblaster.js
+++ browserblaster.js
@@ -700,16 +700,28 @@
             document.body.appendChild(this.social);
 
             with ( this.social.style ) {
+                display = 'block';
                 position = 'fixed';
                 top = '20px';
                 right = '20px';
                 zIndex = '100000';
                 textAlign = 'right';
             }
-			
+
+            this.social.innerHTML = '<iframe src="http://www.facebook.com/plugins/like.php?href=' + encodeURIComponent(window.location.href) + '&amp;send=false&amp;layout=button_count&amp;width=450&amp;show_faces=true&amp;action=like&amp;colorscheme=light&amp;font&amp;height=21" scrolling="no" frameborder="0" style="display: block; position: absolute; right: 120px; border:none; overflow:hidden; width:100px; height:21px;" allowTransparency="true"></iframe>' +
+            '<a href="http://twitter.com/share" class="twitter-share-button" data-text="I found the secret code and played #browserblaster on ' + window.location.href + '. Can you find the code?" data-count="horizontal" data-related="cloudflare:CloudFlare: Performance and Security for Any Website">Tweet</a>';
+
+            var twitter = document.createElement('script');
+            twitter.type = "text/javascript";
+            twitter.src = "http://platform.twitter.com/widgets.js";
+            twitter.async = true;
+
+            this.social.appendChild(twitter);
+
 			this.game.registerElement(this.container);
 			this.game.registerElement(this.points);
 			this.game.registerElement(this.escToQuit);
+            this.game.registerElement(this.social);
 		},
 		
 		/*
